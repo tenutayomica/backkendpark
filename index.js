a3d38acd4d0369bb010cfb4cfdb8221533edd219
tenutayomica <micatenu@gmail.com>
2024-11-19 10:11:07 -0300

    fixcors

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ import express from 'express';
 import router from './usuario/usuariorouter.js';
 import routes from './dibujos/dibujosrouter.js';
 import cors from 'cors'
+app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
 const app = express();
 
 app.use(express.json());
